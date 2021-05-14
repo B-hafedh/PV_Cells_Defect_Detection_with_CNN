06f1a0341cd660ea6a61472d5877a717028db5a7
hafedh BOUGHDIRI <84082577+B-hafedh@users.noreply.github.com>
2021-05-14 17:00:32 +0100

    Update Data_split.py

--- Data_preprocessing/Data_split.py
+++ Data_preprocessing/Data_split.py
@@ -5,4 +5,18 @@ import pandas as pd
 labels_path='/content/cellsData.csv'
 df=pd.read_csv(labels_path)
 
+#split images to infected & uninfected cells according to their infection probability
+infected_list=[]
+uninfected_list=[]
+for index, row in df.iterrows():
+  if row['probability']==0:
+    uninfected_list.append(row['image'][7:])
+  else:
+    infected_list.append(row['image'][7:])
+
+images_path='/content/Original_Data'
+
+
+
+
 
